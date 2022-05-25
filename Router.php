c5d1242265296974f4de68842b04bbcac3508ade
Cristhian Sevilla <crissev17@gmail.com>
2022-05-25 12:37:39 -0500

    method request router.php

--- Router.php
+++ Router.php
@@ -28,17 +28,18 @@ class Router
 
         // $auth = $_SESSION['login'] ?? null;
 
-        $currentUrl = ($_SERVER['REQUEST_URI'] === '') ? '/' :  $_SERVER['REQUEST_URI'] ;
-        $method = $_SERVER['REQUEST_METHOD'];
-            
-        //dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
-        $splitURL = explode('?', $currentUrl);
-        // debuguear($splitURL);
-         
-        if ($method === 'GET') {
-            $fn = $this->getRoutes[$splitURL[0]] ?? null; //$splitURL[0] contiene la URL sin variables 
+        if (isset($_SERVER['PATH_INFO'])) {
+            $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
         } else {
-          $fn = $this->postRoutes[$splitURL[0]] ?? null;
+            $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
+        }
+
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if ($method === 'GET') {
+            $fn = $this->getRoutes[$currentUrl] ?? null;
+        } else {
+            $fn = $this->postRoutes[$currentUrl] ?? null;
         }
 
 
